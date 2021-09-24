d2996bcb4ac183a3bb33e6fd5bdb08afc1987b2a
Jamshid Tashpulatov <44395036+JamshidTashpulatov@users.noreply.github.com>
2021-09-24 15:02:08 +0500

    next app completed

---
+++ scripts/build-rss.js
@@ -0,0 +1,71 @@
+import fs from 'fs'
+import ReactDOMServer from 'react-dom/server'
+import { MDXProvider } from '@mdx-js/react'
+import { Feed } from 'feed'
+
+import { getAllPosts } from '../src/getAllPostPreviews'
+
+const siteUrl = '#'
+
+const feed = new Feed({
+  title: 'Jamshidbek Tashpulatov',
+  description: 'Personal blog',
+  id: siteUrl,
+  link: siteUrl,
+  language: 'en',
+  image: `${siteUrl}/favicon-32x32.png`,
+  favicon: `${siteUrl}/favicon.ico`,
+  copyright: `All rights reserved ${new Date().getFullYear()}, Jamshidbek Tashpulatov`,
+  feedLinks: {
+    rss: `${siteUrl}/feed.xml`,
+    json: `${siteUrl}/feed.json`,
+    atom: `${siteUrl}/atom.xml`,
+  },
+  author: {
+    name: 'Jamshidbek Tashpulatov',
+    link: 'https://twitter.com/@heyjamshid',
+  },
+})
+
+getAllPosts().forEach(({ link, module: { meta, default: Content } }) => {
+  const mdx = (
+    <MDXProvider>
+      <Content />
+    </MDXProvider>
+  )
+  const html = ReactDOMServer.renderToStaticMarkup(mdx)
+  const postText = `<p><em>(The post <a href="${siteUrl + link}">${
+    meta.title
+  }</a> appeared first on <a href="${siteUrl}">Jamshidbek Tashpulatov Personal Blog</a>.)</em></p>`
+  feed.addItem({
+    title: meta.title,
+    id: meta.title,
+    link,
+    description: meta.description,
+    content: html + postText,
+    author: meta.authors.map(({ name, twitter }) => ({
+      name,
+      link: `https://twitter.com/${twitter}`,
+    })),
+    date: new Date(meta.date),
+    image: siteUrl + meta.image,
+    ...(meta.discussion
+      ? {
+          comments: meta.discussion,
+          extensions: [
+            {
+              name: '_comments',
+              objects: {
+                about: 'Link to discussion forum',
+                comments: meta.discussion,
+              },
+            },
+          ],
+        }
+      : {}),
+  })
+})
+
+fs.writeFileSync('./out/feed.xml', feed.rss2())
+fs.writeFileSync('./out/atom.xml', feed.atom1())
+fs.writeFileSync('./out/feed.json', feed.json1())
